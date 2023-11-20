6b98a4015768bef097e5bb30dd5805ff3bb1111a
unknown <khan.farhan1985@gmail.com>
2023-11-20 07:40:35 +0530

    [Added new Endpoint]

--- src/main/java/com/example/demodockerjenkins/DemoDockerJenkinsApplication.java
+++ src/main/java/com/example/demodockerjenkins/DemoDockerJenkinsApplication.java
@@ -2,12 +2,20 @@ package com.example.demodockerjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoDockerJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoDockerJenkinsApplication.class, args);
+	}
+	
+	@GetMapping(path = "/welcome")
+	public String welcomeMessage() {
+		return "Welcome, this is first demo of docker and jenkins";
 	}
 
 }
