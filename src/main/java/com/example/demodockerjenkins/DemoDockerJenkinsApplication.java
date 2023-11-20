68b9992e527b5339d891ecf840bcfcc594aea617
unknown <khan.farhan1985@gmail.com>
2023-11-20 07:35:02 +0530

    [Initial Commit]

---
+++ src/main/java/com/example/demodockerjenkins/DemoDockerJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodockerjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demodockerjenkins/DemoDockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodockerjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
